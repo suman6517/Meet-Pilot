17df562b83c27652a7f45857df2ea595a1217644
Suman Baral <ssumanbaral55@gmail.com>
2025-12-09 20:30:27 +0530

    12 : Agent Data Table and design done

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -3,6 +3,10 @@ import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 import { useTRPC } from "@/trpc/client";
 import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
+import { DataTable } from "../components/data-table";
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
+
 
 export const AgentView =() =>{
     const trpc = useTRPC();
@@ -26,10 +30,16 @@ export const AgentView =() =>{
     //         description="Please try again later"
     //         />
     //     )
-    // }
+    // } 
     return(
-        <div>
-            {JSON.stringify(data, null, 2)}
+        <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4 ">
+            <DataTable data={data} columns={columns}/>
+            {data.length === 0 && (
+                <EmptyState
+                title="Create Your First Agent"
+                description="Create an agent to join your Mettings. Each agent will follow your instructions and can interact with particepant during the call."
+                />
+            )}
         </div>
     );
 };
