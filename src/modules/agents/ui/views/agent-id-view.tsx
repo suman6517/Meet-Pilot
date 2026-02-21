0deb46057df8d52d7bc003ca9c4a6de6bfe0eb3f
Suman Baral <ssumanbaral55@gmail.com>
2026-02-22 00:32:36 +0530

    20: Meeting Page Implement Meeting Management Features
    - Enhanced the meeting details page with authentication checks and loading/error states.
    - Introduced a confirmation dialog for meeting removal actions.
    - Added components for editing and updating meeting details.
    - Implemented a new header component for better navigation within the meeting details view.
    - Updated server procedures to support meeting removal and improved data fetching logic.

--- src/modules/agents/ui/views/agent-id-view.tsx
+++ src/modules/agents/ui/views/agent-id-view.tsx
@@ -9,7 +9,7 @@ import { Badge } from "@/components/ui/badge";
 import { VideoIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
-import { useConfirm } from "../../hooks/use-confirm";
+import { useConfirm } from "@/hooks/use-confirm";   // This got Modified
 import{useState} from "react";
 import { UpdateAgentDialog } from "../components/update-agent-dialog";
 
--- src/modules/meetings/ui/components/update-meeting-dialog.tsx
+++ src/modules/meetings/ui/components/update-meeting-dialog.tsx
@@ -1,30 +1,31 @@
 import { ResponsiveDialog } from "@/components/reponsive-dilog";
 import { MeetingForm } from "./meeting-form";
 import { useRouter } from "next/navigation";
+import { MeetingGetOne } from "../../types";
 
-interface newMeetingDialogProps{
+interface UpdateMeetingDialogProps{
     open:boolean;
     onOpenChange:(open:boolean) =>void;
+    initialValues:MeetingGetOne;
 }
 
-export const NewMeetingDialog = ({
+export const UpdateMeetingDialog = ({
     open,
-    onOpenChange
-}:newMeetingDialogProps)=>{
+    onOpenChange,
+    initialValues
+}:UpdateMeetingDialogProps)=>{
     const router =useRouter();
     return(
         <ResponsiveDialog
-        title="New Meeting"
-        description="Create a new Meeting"
+        title="Edit Meeting"
+        description="Edit the meeting details"
         open={open}
         onOpenChange={onOpenChange}
         >
             <MeetingForm
-            onSuccess={(id) => {
-                onOpenChange(false);
-                router.push(`/meetings/${id}`);
-            }}
+            onSuccess={() => {onOpenChange(false)}}
             onCancel={() => onOpenChange(false)}
+            initialValues={initialValues}
             />
         </ResponsiveDialog>
     )                                                                                   
