214da9dffa872f1ffe4dce96db41a3f68ace7ac9
Suman Baral <ssumanbaral55@gmail.com>
2026-02-22 05:20:53 +0530

    21: Meeting Varriants Add meeting state components and SVG icons
    - Introduced new SVG icons for meeting states: cancelled, processing, and upcoming.
    - Created components for handling different meeting states: ActiveState, CancelledState, ProcessingState, and UpcomingState.
    - Updated EmptyState component to accept a dynamic image prop for better reusability in various states.
    - Integrated new state components into the MeetingIdView for improved user experience.

--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -9,6 +9,10 @@ import { useRouter } from "next/navigation";
 import {useConfirm} from "@/hooks/use-confirm"
 import { UpdateMeetingDialog } from "../components/update-meeting-dialog";
 import { useState } from "react";
+import { UpcomingState } from "../components/upcoming-state";
+import { ActiveState } from "../components/active-state";
+import { CancelledState } from "../components/cancelled-state";
+import { ProcessingState } from "../components/processing-state";
 
 interface Props {
      meetingId:string
@@ -53,6 +57,14 @@ export const MeetingIdView = ({meetingId}:Props) =>
         await removeMeeting.mutateAsync({id:meetingId});
     }
 
+    const isActive = data.status === "active";
+    const isUpcoming = data.status === "upcoming";
+    const isCancelled = data.status === "cancled"
+    const isCompleted = data.status === "complited"
+    const isProcessing = data.status === "processing"
+
+
+
     return(
         <>
         <RemoveConfirmation/>
@@ -68,7 +80,21 @@ export const MeetingIdView = ({meetingId}:Props) =>
             onEdit={() =>setupdateMeetingDilogOpen(true)}
             onRemove={handleremoveMeeting}
             />
-            {JSON.stringify(data, null ,2)}
+            {isCancelled && <CancelledState/>
+            }
+            {isProcessing && <ProcessingState/>}
+            
+            {isCompleted && <div>Completed</div>}
+
+            {isActive && <ActiveState
+            meetingId={meetingId}
+            />}
+
+            {isUpcoming && (<UpcomingState
+            meetingId={meetingId}
+            onCancelMeeting={() => {}}
+            isCancelling ={false}
+            />)}
         </div>
         </>
     )
