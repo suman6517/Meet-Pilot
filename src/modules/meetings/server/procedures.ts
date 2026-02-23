e0d41cf03eb978006cf57d4675bf13ed36696166
Suman Baral <ssumanbaral55@gmail.com>
2026-02-23 07:44:44 +0530

    22:Video Call (Stream)Add video call functionality and related components
    - Introduced new components for video call management, including CallProvider, CallConnect, CallLobby, CallActive, CallEnded, and CallUi.
    - Integrated Stream Video SDK for real-time video calling capabilities.
    - Implemented user authentication and session management for call participants.
    - Added avatar generation utility for user images.
    - Updated package dependencies to include @stream-io/node-sdk and @stream-io/video-react-sdk.
    - Enhanced meeting procedures to support video call creation and user token generation.

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -8,8 +8,36 @@ import { TRPCError } from "@trpc/server";
 import { mettingInsertSchema, mettingsUpdateSchema } from "../schemas";
 import { duration } from "drizzle-orm/gel-core";
 import { MeetingStatus } from "../types";
+import { streamVideo } from "@/lib/stream-video";
+import { generateAvaterUri } from "@/lib/avater";
 
 export const meetingsRouter = createTRPCRouter ({
+
+
+    generateToken: protectedProcedure.mutation(async ({ctx}) =>{
+        await streamVideo.upsertUsers([
+            {
+                id: ctx.auth.user.id,
+                name:ctx.auth.user.name,
+                role: "admin",
+                image:
+                    ctx.auth.user.image ??
+                    generateAvaterUri({seed : ctx.auth.user.name , varriant: "initials"}),
+            },
+        ]);
+
+        const expirationTime = Math.floor(Date.now() /1000) +3600; // 1 hour
+        const issuedAt = Math.floor(Date.now() /1000) -60;
+
+        const token = streamVideo.generateUserToken({
+            user_id:ctx.auth.user.id,
+            exp:expirationTime,
+            validity_in_seconds:issuedAt
+        })
+
+        return token;
+    }),
+
 
     remove:protectedProcedure
     .input(z.object({id:z.string() }))
@@ -70,6 +98,54 @@ export const meetingsRouter = createTRPCRouter ({
 
 
         //Todo: - Creat Stream Call , Upseart Stream User
+        const call = streamVideo.video.call("default" , createdMeeting.id);
+
+        await call.create({
+            data:{
+                created_by_id: ctx.auth.user.id,
+                custom:{
+                    meetingId:createdMeeting.id,
+                    meetingName: createdMeeting.name
+                },
+                settings_override:{
+                    transcription:{
+                        language:"en",
+                        mode:"auto-on",
+                        closed_caption_mode:"auto-on"
+                    },
+                recording:{
+                    mode:"auto-on",
+                    quality:"1080p",
+                },
+                },
+            },
+        });
+
+        const[existingAgent] = await db
+        .select()
+        .from(agents)
+        .where(eq(agents.id, createdMeeting.agentId));
+
+
+        if(!existingAgent)
+        {
+            throw new TRPCError({
+                code:"NOT_FOUND",
+                message: "Agent not found"
+            });
+
+            await streamVideo.upsertUsers([
+                {
+                    id:existingAgent.id,
+                    name:existingAgent.name,
+                    role:"user",
+                    image:generateAvaterUri({
+                        seed:existingAgent.name,
+                        varriant:"botttsNeutral",
+                    })
+                }
+            ]);
+        }
 
          return createdMeeting;
     }),
