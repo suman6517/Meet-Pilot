1f2d5173f9c8d99f7f990e17ae918839376ea5e9
Suman Baral <ssumanbaral55@gmail.com>
2026-05-20 02:55:34 +0530

    24: Background Job(Inngest , OpenAi) Update package dependencies and implement Inngest integration
    - Added @inngest/agent-kit and related dependencies to package.json and package-lock.json.
    - Created Inngest client for event handling in src/inngest/client.ts.
    - Developed meetingsProcessing function in src/inngest/functions.ts to handle meeting transcript summarization.
    - Implemented new API routes for Inngest in src/app/api/inngest/route.ts.
    - Enhanced webhook route to send events to Inngest upon call session updates in src/app/api/webhook/route.ts.
    - Updated MeetingStatus enum and added StreamTranscriptItem type in src/modules/meetings/types.ts.

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,8 +1,8 @@
 import{
-    //CallEndedEvent,
-    //CallTranscriptionReadyEvent,
+    CallEndedEvent,
+    CallTranscriptionReadyEvent,
     CallSessionParticipantLeftEvent,
-    //CallRecordingReadyEvent,
+    CallRecordingReadyEvent,
     CallSessionStartedEvent
 } from "@stream-io/node-sdk";
 
@@ -15,6 +15,7 @@ import { agents , meetings } from "@/db/schema";
 import {streamVideo} from "@/lib/stream-video";
 import { error } from "console";
 import OpenAI from "openai";
+import { inngest } from "@/inngest/client";
 
 function verifySignatureWithSDK (body : string , signature: string)
 {
@@ -169,8 +170,74 @@ export async function POST(req:NextRequest)
         await call.end();
     }
 
+    // This is from Chap 24
+
+    else if(eventType === "call.session_ended")
+    {
+        const event = payload as CallEndedEvent;
+        const meetingId = event.call.custom?.meetingId;
+
+        if(!meetingId)
+        {
+            return NextResponse.json({error:"Missing MeetingId"} , {status:400});
+        }
+
+        await db
+        .update(meetings)
+        .set({
+            status:"processing",
+            endedAt:new Date(),
+        })
+        .where(and(eq(meetings.id , meetingId) , eq(meetings.status , "active")));
+    }
+
+    
+    else if(eventType === "call.transcription_ready")
+    {
+        const event = payload as CallTranscriptionReadyEvent;
+        const meetingId = event.call_cid.split(":")[1];
+
+        const [updateMeeting] = await db
+        .update(meetings)
+        .set({
+            transcriptUrl: event.call_transcription.url,
+        })
+        .where(eq(meetings.id , meetingId))
+        .returning();
+
+
+        if(!updateMeeting)
+        {
+            return NextResponse.json({error:"Meeting not found"} , {status:404});
+        }
+
+        // TODO: Call Ingese background job to summarize the transcript
+        await inngest.send({
+            name: "meetings/processing",
+            data:{
+                meetingId: updateMeeting.id,
+                transcriptUrl: updateMeeting.transcriptUrl,
+            },
+        })
+    }
+
+    else if(eventType === "call.recording_ready")
+    {
+        const event = payload as CallRecordingReadyEvent;
+        const meetingId = event.call_cid.split(":")[1];
+
+        await db
+        .update(meetings)
+        .set({
+            recordingUrl: event.call_recording.url,
+        })
+        .where(eq(meetings.id , meetingId))
+    }
+
 
     return NextResponse.json({status : "ok"});
 
 
 }
+
+// http://localhost:8288 :- Inngest Localhost
--- src/modules/meetings/types.ts
+++ src/modules/meetings/types.ts
@@ -9,4 +9,12 @@ export enum MeetingStatus {
     Completed = "complited",
     Processing = "processing",
     Cancelled = "cancled",
-}
+};
+
+export type StreamTranscriptItem = {
+    speaker_id:string;
+    type:string;
+    text:string;
+    start_ts:number;
+    stop_ts:number;
+};
