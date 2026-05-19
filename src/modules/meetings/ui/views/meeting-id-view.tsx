8b1cab9fae73dca9f49157be15d7db15537e4901
Suman Baral <ssumanbaral55@gmail.com>
2026-05-20 05:19:58 +0530

    25: Completed State

--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -13,6 +13,7 @@ import { UpcomingState } from "../components/upcoming-state";
 import { ActiveState } from "../components/active-state";
 import { CancelledState } from "../components/cancelled-state";
 import { ProcessingState } from "../components/processing-state";
+import { CompletedState } from "../components/completed-state";
 
 interface Props {
      meetingId:string
@@ -80,11 +81,9 @@ export const MeetingIdView = ({meetingId}:Props) =>
             onEdit={() =>setupdateMeetingDilogOpen(true)}
             onRemove={handleremoveMeeting}
             />
-            {isCancelled && <CancelledState/>
-            }
+            {isCancelled && <CancelledState/>}
             {isProcessing && <ProcessingState/>}
-            
-            {isCompleted && <div>Completed</div>}
+            {isCompleted && <CompletedState data = {data}/>}
 
             {isActive && <ActiveState
             meetingId={meetingId}
